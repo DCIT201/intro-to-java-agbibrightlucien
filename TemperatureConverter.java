9fbfe3aa9b301f1d6d26573a28c629b3bdb1eaaf
Bright Lucien Jnr Agbi <agbibrightlucienjnr@gmail.com>
2024-12-08 04:05:52 +0000

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,42 +1,66 @@
 import java.util.Scanner;
 
 public class TemperatureConverter {
-    public static void main(String[] args) {
-        Scanner converter = new Scanner(System.in);
+    private static final double ABSOLUTE_ZERO_CELSIUS = -273.15;
+    private static final double ABSOLUTE_ZERO_FAHRENHEIT = -459.67;
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("A Simple Temperature Converter");
         System.out.println("Please enter your first name:");
-        String firstName = converter.nextLine();
+        String firstName = scanner.nextLine();
         System.out.println("Welcome " + firstName + " to my Temperature Converter!");
-        System.out.println("Please choose between the two options below:");
-        System.out.println("Option a: Convert from Celsius to Fahrenheit");
-        System.out.println("Option b: Convert from Fahrenheit to Celsius");
 
-        String option = converter.nextLine();
+        while (true) {
+            System.out.println("Please choose between the two options below:");
+            System.out.println("Option a: Convert from Celsius to Fahrenheit");
+            System.out.println("Option b: Convert from Fahrenheit to Celsius");
+            System.out.println("Option q: Quit");
 
-        if (option.equalsIgnoreCase("a")) {
-            System.out.println("Input the desired temperature you want to convert to Fahrenheit:");
-            double celsius = converter.nextDouble();
-            if (celsius < -273.15) {
-                System.out.println("The temperature cannot be lower than -273.15 Celsius.");
+            String option = scanner.nextLine();
+
+            if (option.equalsIgnoreCase("a")) {
+                convertCelsiusToFahrenheit(scanner);
+            } else if (option.equalsIgnoreCase("b")) {
+                convertFahrenheitToCelsius(scanner);
+            } else if (option.equalsIgnoreCase("q")) {
+                break;
             } else {
-                double fahrenheit = (celsius * 9 / 5) + 32;
-                System.out.println("The temperature in Fahrenheit = " + fahrenheit);
+                System.out.println("Invalid option. Please choose 'a', 'b', or 'q'.");
             }
-        } else if (option.equalsIgnoreCase("b")) {
-            System.out.println("Input the desired temperature you want to convert to Celsius:");
-            double fahrenheit = converter.nextDouble();
-            if (fahrenheit < -459.67) {
-                System.out.println("The temperature cannot be lower than -459.67 Fahrenheit.");
-            } else {
-                double celsius = (fahrenheit - 32) * 5 / 9;
-                System.out.println("The temperature in Celsius = " + celsius);
-            }
-        } else {
-            System.out.println("Invalid option. Please choose 'a' or 'b'.");
         }
 
         System.out.println("THANK YOU FOR USING THE TEMPERATURE CONVERTER.");
-        converter.close();
+        scanner.close();
+    }
+
+    private static void convertCelsiusToFahrenheit(Scanner scanner) {
+        System.out.println("Input the desired temperature you want to convert to Fahrenheit:");
+        double celsius = getValidTemperatureInput(scanner);
+        if (celsius < ABSOLUTE_ZERO_CELSIUS) {
+            System.out.println("The temperature cannot be lower than -273.15 Celsius.");
+        } else {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            System.out.println("The temperature in Fahrenheit = " + fahrenheit);
+        }
+    }
+
+    private static void convertFahrenheitToCelsius(Scanner scanner) {
+        System.out.println("Input the desired temperature you want to convert to Celsius:");
+        double fahrenheit = getValidTemperatureInput(scanner);
+        if (fahrenheit < ABSOLUTE_ZERO_FAHRENHEIT) {
+            System.out.println("The temperature cannot be lower than -459.67 Fahrenheit.");
+        } else {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            System.out.println("The temperature in Celsius = " + celsius);
+        }
+    }
+
+    private static double getValidTemperatureInput(Scanner scanner) {
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Invalid input. Please enter a valid number:");
+            scanner.next();
+        }
+        return scanner.nextDouble();
     }
 }
